84184bb64af3f68a547c37781804d7048535df05
hathuyvnnd <hathuyvnnd@gmail.com>
2025-03-10 23:01:14 +0700

    first commit

---
+++ src/test/java/com/example/maintenece_motorcycle_management/MainteneceMotorcycleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenece_motorcycle_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainteneceMotorcycleManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
