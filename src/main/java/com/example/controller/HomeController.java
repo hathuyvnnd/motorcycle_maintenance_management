c0ef07a60e7d28463b57897f8036d2386d066684
kaizz0105 <nothing010598@gmail.com>
2025-03-21 22:29:22 +0700

    sửa lại đường dẫn giao diện index.html của customer

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = {"/", "/{path:[^\\.]*}"})
+    @RequestMapping(value = { "/", "/{path:[^\\.]*}" })
     public String redirect() {
-        return "forward:/index.html";
+        return "forward:/customer/index.html";
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,65 +1,62 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
-app.config(function($routeProvider){
-    $routeProvider
-    .when("/",{
-        templateUrl: "views/trangchu.html",
-        controller: "HomeController",
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "customer/views/trangchu.html",
+      controller: "HomeController",
     })
-    .when("/dichvu",{
-        templateUrl:"views/dichvu.html",
-        controller:"DichVuController",
+    .when("/dichvu", {
+      templateUrl: "customer/views/dichvu.html",
+      controller: "DichVuController",
     })
-    .when("/phutung",{
-        templateUrl:"views/phutung.html",
-        controller:"PhuTungController",
+    .when("/phutung", {
+      templateUrl: "customer/views/phutung.html",
+      controller: "PhuTungController",
     })
-    .when("/gioithieu",{
-        templateUrl:"views/gioithieu.html",
-        controller:"GioiThieuController",
+    .when("/gioithieu", {
+      templateUrl: "customer/views/gioithieu.html",
+      controller: "GioiThieuController",
     })
-    .when("/datlich",{
-        templateUrl:"views/datLichHen.html",
-        controller:"DatLichController",
+    .when("/datlich", {
+      templateUrl: "customer/views/datLichHen.html",
+      controller: "DatLichController",
     })
-    .when("/dangki",{
-        templateUrl:"views/dangki.html",
-        controller:"DangKiController",
+    .when("/dangki", {
+      templateUrl: "customer/views/dangki.html",
+      controller: "DangKiController",
     })
-    .when("/dangnhap",{
-        templateUrl:"views/dangnhap.html",
-        controller:"DangNhapController",
+    .when("/dangnhap", {
+      templateUrl: "customer/views/dangnhap.html",
+      controller: "DangNhapController",
     })
-    .when("/quenmatkhau",{
-        templateUrl:"views/quenmatkhau.html",
-        controller:"QuenMatKhauController",
-    })
-
-
-    
+    .when("/quenmatkhau", {
+      templateUrl: "customer/views/quenmatkhau.html",
+      controller: "QuenMatKhauController",
+    });
 });
 
-app.controller("HomeController",function($scope){
-    $scope.title = "Trang Chủ";
+app.controller("HomeController", function ($scope) {
+  $scope.title = "Trang Chủ";
 });
-app.controller("DichVuController",function($scope){
-    $scope.title = "Dịch Vụ";
+app.controller("DichVuController", function ($scope) {
+  $scope.title = "Dịch Vụ";
 });
-app.controller("PhuTungController",function($scope){
-    $scope.title = "Phụ Tùng";
-})
-app.controller("GioiThieuController",function($scope){
-    $scope.title = "Giới Thiệu";
-})
-app.controller("DatLichController",function($scope){
-    $scope.title = "Đặt Lịch Hẹn";
-})
-app.controller("DangKiController",function($scope){
-    $scope.title = "Đăng Kí";
-})
-app.controller("DangNhapController",function($scope){
-    $scope.title = "Đăng Nhập";
-})
-app.controller("QuenMatKhauController",function($scope){
-    $scope.title = "Quên Mật Khẩu";
-})
+app.controller("PhuTungController", function ($scope) {
+  $scope.title = "Phụ Tùng";
+});
+app.controller("GioiThieuController", function ($scope) {
+  $scope.title = "Giới Thiệu";
+});
+app.controller("DatLichController", function ($scope) {
+  $scope.title = "Đặt Lịch Hẹn";
+});
+app.controller("DangKiController", function ($scope) {
+  $scope.title = "Đăng Kí";
+});
+app.controller("DangNhapController", function ($scope) {
+  $scope.title = "Đăng Nhập";
+});
+app.controller("QuenMatKhauController", function ($scope) {
+  $scope.title = "Quên Mật Khẩu";
+});
