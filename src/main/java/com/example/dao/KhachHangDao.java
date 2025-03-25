685b4e10d0c882138056e2a39158586170f86fa5
ngoanhmam1270 <101687757+ngoanhmam1270@users.noreply.github.com>
2025-03-25 23:30:51 +0700

    Delete src/main/java/com/example/dao directory

--- src/main/java/com/example/dao/KhachHangDao.java
+++
@@ -1,8 +0,0 @@
-package com.example.dao;
-
-import com.example.model.KhachHang;
-import com.example.model.LoaiXe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KhachHangDao extends JpaRepository<KhachHang,String> {
-}
