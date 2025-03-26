083189377fae66d072e242c7574b22a29daf7ed3
hathuyvnnd <hathuyvnnd@gmail.com>
2025-03-26 15:58:11 +0700

    Load dữ liệu phụ tùng, lọc theo giá, phân trang, lọc theo tên

--- src/main/java/com/example/service/LoaiPhuTungService.java
+++ src/main/java/com/example/service/LoaiPhuTungService.java
@@ -1,7 +1,9 @@
 package com.example.service;
 
+import java.util.List;
+
 import com.example.model.LoaiPhuTung;
 
 public interface LoaiPhuTungService extends CrudService<LoaiPhuTung, String> {
-
+    public LoaiPhuTung findById(String idLoaiPT);
 }
--- src/main/java/com/example/service_impl/PhuTungServiceImpl.java
+++ src/main/java/com/example/service_impl/PhuTungServiceImpl.java
@@ -4,16 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.dao.PhuTungDao;
+import com.example.model.LoaiPhuTung;
 import com.example.model.PhuTung;
 import com.example.service.PhuTungService;
 
 import java.util.List;
-@Service("PhuTungService")
+@Service("phuTungService")
 public class PhuTungServiceImpl implements PhuTungService{
     @Autowired
     private PhuTungDao pTungDao;
 
-    public List<PhuTung> findAll(){
-        return pTungDao.findAll();
-    }
+
+@Override
+public List<PhuTung> findByLoaiPT(LoaiPhuTung loaiPT) {
+    System.out.println("🔍 Đang tìm phụ tùng theo loại: " +loaiPT.getIdLoaiPT());
+    List<PhuTung> list = pTungDao.findByLoaiPT(loaiPT);
+    System.out.println("✅ Số phụ tùng tìm thấy: " + list.size());
+    return list;
 }
+
+}
+
